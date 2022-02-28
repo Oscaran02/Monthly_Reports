484a7fd8d458f12c716e371845d5051692e7a407
Oscar Pacheco <oscarpa_2000@hotmail.com>
2022-02-28 13:33:59 -0500

    some corrections

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-# Evaluar el mes de febrero en el 2022 para obtener resultados con el csv de prueba
+# Evaluate the month of February in 2022 to obtain results with the test csv.
 
 from datetime import datetime
-import pandas as pd
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from matplotlib.dates import DateFormatter, DayLocator
 
 
-def csv_import():
-    # Formato de fecha
+def get_data_from_csv():
+    # Date format
     custom_date_parser = lambda x: datetime.strptime(x, "%d-%m-%Y %H:%M:%S")
 
-    # Importar de datos desde el csv y convertirlos en formato de fecha
+    # Import data from csv and convert to date format
     df = pd.read_csv('data.csv',
                      parse_dates=['arrival_date', 'departure_date'],
                      date_parser=custom_date_parser)
@@ -21,63 +22,67 @@ def csv_import():
 
 
 def dates_from_month(month, year):
-    # Formato para fecha inicial y fecha final
-    start_date = str(year) + '-' + str(month) + '-1'
+    # Format for start date and end date
+    start_date = f"{year}-{month}-1"
     start_date = pd.to_datetime(start_date)
-    # En caso de que sea el último mes del año
+    # In case it is the last month of the year
     if month == 12:
-        stop_date = str(year + 1) + '-1-1'
+        stop_date = f"{year+1}-1-1"
     else:
-        stop_date = str(year) + '-' + str(month + 1) + '-1'
+        stop_date = f"{year}-{month+1}-1"
     stop_date = pd.to_datetime(stop_date)
 
     return np.arange(start_date, stop_date, dtype='datetime64[D]')
 
-
-def consolidado_mensual(month, year):
-    # Importar el dataframe extraído del csv
-    df = csv_import()
-
-    # Crear array de fechas en el mes seleccionado
-    days_array = dates_from_month(month, year)
-
-    # Llenar el número de pedidos activos por días en el mes seleccionado
-    pedidos_activos = []
-    for day in days_array:
-        sum = 0
-        for i in range(len(df)):
-            if df['arrival_date'][i] <= day <= df['departure_date'][i]:
-                sum += 1
-        pedidos_activos.append(sum)
-
-    # Obtener el nombre completo del mes
+def graphing_statistics(month, year, pedidos_activos, days_array):
+    # Get the full name of the month
     datetime_object = datetime.strptime(str(month), "%m")
     month_name = datetime_object.strftime("%B")
 
-    # Graficar estadísticas
+    # Graphing statistics
     date_form = DateFormatter("%d-%m")
     fig, ax = plt.subplots(figsize=(15, 8))
     ax.plot(days_array, pedidos_activos, label=month_name, marker='o')
     ax.legend(loc='upper right')
-    ax.set_ylabel('Número de paquetes activos')
-    ax.set_xlabel('Día del mes')
-    ax.set_title("Número de paquetes en bodega por día")
+    ax.set_ylabel('Number of active packages')
+    ax.set_xlabel('Day of the month')
+    ax.set_title(f"Monthly Consolidated\n{month_name} - {year}")
     ax.xaxis.set_major_formatter(date_form)
     ax.xaxis.set_major_locator(DayLocator(interval=2))
     ax.grid(axis='y', color='gray', linestyle='dashed')
     plt.show()
 
 
+def monthly_consolidated(month, year):
+    # Import the dataframe extracted from csv
+    dataframe = get_data_from_csv()
+
+    # Create an array of dates in the selected month
+    days_array = dates_from_month(month, year)
+
+    # Fill in the number of active orders per days in the selected month
+    pedidos_activos = []
+    for day in days_array:
+        sum = 0
+        for i in range(len(dataframe)):
+            if dataframe['arrival_date'][i] <= day <= dataframe['departure_date'][i]:
+                sum += 1
+        pedidos_activos.append(sum)
+
+    # Graphing statistics
+    graphing_statistics(month, year, pedidos_activos, days_array)
+
+
 if __name__ == '__main__':
-    # Validación de datos
+    # Data validation
     while True:
         try:
-            mes = int(input("Escriba el número del mes a evaluar: "))
-            anio = int(input("Escriba el número del año a evaluar: "))
-            if 1 <= mes <= 12:
+            month = int(input("Enter the number of the month to be evaluated: "))
+            year = int(input("Enter the number of the year to be evaluated: "))
+            if 1 <= month <= 12 and 1 <= year <= 9999:
                 break
             else:
-                print('Ingrese un número de mes valido')
+                print('Enter a valid month or year number')
         except ValueError:
             print("Oops!  That was no valid number.  Try again...")
-    consolidado_mensual(mes, anio)
+    monthly_consolidated(month, year)
